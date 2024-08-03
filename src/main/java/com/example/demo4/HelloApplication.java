d0cf23c966dbd1e33fff44dd0d9e143ae06fe6a5
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2024-08-03 11:35:49 -0400

    affichage image

--- src/main/java/com/example/demo4/HelloApplication.java
+++ src/main/java/com/example/demo4/HelloApplication.java
@@ -1,23 +1,46 @@
 package com.example.demo4;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!"); //test
-        stage.setScene(scene);
-        stage.show();
-    }
 
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        Image image = new Image("logo.png");
+        
+        ImageView iv = new ImageView(image);
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        root.getChildren().add(iv);
+
+        Scene scene = new Scene(root, 500, 450);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
